523556017b63b5366db9ca8edbde3cc99efc50e7
Dafnik <git@dafnik.me>
2025-08-11 17:16:25 +0200

    feat: add missing compare operator

--- src/test/kotlin/me/dafnik/jpaSpecificationBuilderTests/models/Department.kt
+++ src/test/kotlin/me/dafnik/jpaSpecificationBuilderTests/models/Department.kt
@@ -1,13 +1,18 @@
 package me.dafnik.jpaSpecificationBuilderTests.models
 
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 
 @Entity
 data class Department(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val name: String? = null
+    val name: String? = null,
+
+    @OneToMany(fetch = FetchType.EAGER)
+    val users: List<User> = emptyList(),
 )
